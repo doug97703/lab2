1eb33a12a8cb0a6a3e6d24fa87378eb77062724f
doug97703 <douglasjtaylor94@gmail.com>
2019-09-25 14:39:41 -0700

    bugs fixed, changed console logs to alerts

--- js/app.js
+++ js/app.js
@@ -1,37 +1,44 @@
 'use strict';
-// var userName = prompt('please give me your name')
-// console.log('hello ' + userName + ' , I hope your day is going well.')
-// var skyIsBlue = prompt('quick question - is the sky blue?')
-// if (skyIsBlue.toLowerCase() === 'yes' || skyIsBlue.toLowerCase() === 'y'){
-//     console.log('hmm that sounds right to me!');
-// }
-// else{
-//     console.log("that's an interesting response, which planet are you from?")
-// }
+var numberCorrect = 0;
 
-// var romeCapital = prompt('did Rome become the capital of a united Italian state in 1867?')
-// if (romeCapital.toLowerCase() === 'no' || skyIsBlue.toLowerCase() === 'n') {
-//     console.log('correct! It was 1870. That\'s quite the trivia you know. You would be good on Jeopardy.');
-// }
-// else {
-//     console.log("actually, it wasn't until a plebiscite in October 1870 that Rome became the capital of a united Italy.")
-// }
+var userName = prompt('please give me your name')
+alert('hello ' + userName + ' , I hope your day is going well.')
+var skyIsBlue = prompt('quick question - is the sky blue?')
+if (skyIsBlue.toLowerCase() === 'yes' || skyIsBlue.toLowerCase() === 'y'){
+    alert('hmm that sounds right to me!');
+    numberCorrect += 1;
+}
+else{
+    alert("that's an interesting response, which planet are you from?")
+}
 
-// var congressAge = prompt('help me remember. What|\'s the required age for a federal congressperson in the United States? Is it 35?')
-// if (congressAge.toLowerCase() === 'no' || skyIsBlue.toLowerCase() === 'n') {
-//     console.log('I think you\'re right. It\'s 25.');
-// }
-// else {
-//     console.log('no, i think it\'s 25, actually.');
-// }
+var romeCapital = prompt('did Rome become the capital of a united Italian state in 1867?')
+if (romeCapital.toLowerCase() === 'no' || skyIsBlue.toLowerCase() === 'n') {
+    alert('correct! It was 1870. That\'s quite the trivia you know. You would be good on Jeopardy.');
+    numberCorrect += 1;
+}
+else {
+    alert("actually, it wasn't until a plebiscite in October 1870 that Rome became the capital of a united Italy.")
+}
 
-// var airConditionerYear = prompt('when Charles Gates installed the first ever in-home air conditioner, what year was it? 1914?')
-// if (congressAge.toLowerCase() === 'yes' || skyIsBlue.toLowerCase() === 'y') {
-//     console.log('that\'s right. he must have been super rich!');
-// }
-// else {
-//     console.log('nope, it actually was 1914.');
-//
+var congressAge = prompt('help me remember. What|\'s the required age for a federal congressperson in the United States? Is it 35?')
+if (congressAge.toLowerCase() === 'no' || skyIsBlue.toLowerCase() === 'n') {
+    alert('I think you\'re right. It\'s 25.');
+    numberCorrect += 1;
+}
+else {
+    alert('no, i think it\'s 25, actually.');
+}
+
+var airConditionerYear = prompt('when Charles Gates installed the first ever in-home air conditioner, what year was it? 1913?')
+if (congressAge.toLowerCase() === 'yes' || skyIsBlue.toLowerCase() === 'y') {
+    alert('that\'s right. he must have been super rich!');
+    numberCorrect += 1;
+}
+else {
+    alert('nope, it actually was 1914.');
+}
+
 
 
 var number = 47;
@@ -42,6 +49,7 @@ do {
     var numberGuess = prompt("please guess a number, any number");
     if (number == numberGuess) {
         alert('hey that is the number i was thinking of!');
+        numberCorrect += 1;
         break;
     }
     else if (numberGuess > number) {
@@ -58,13 +66,13 @@ do {
 
 var attempt = 0
 do {
-    
     var vehicleGuess = prompt('what is a type of vehicle?');
     var z = 0
     var answerArray = ['sedan', 'truck', 'car', 'suv', 'van'];
     while (z < answerArray.length) {
         if (vehicleGuess == answerArray[z]) {
             alert('that answer is correct')
+            numberCorrect += 1;
             var correctAnswer = true;
             break;
         }
@@ -81,32 +89,4 @@ do {
     }
 } while (attempt < 6);
 
-
-
-// for(var i = 0; i = 5; i++) {
-//     var vehicleGuess = prompt('what is a type of vehicle?');
-//     var z = 0;
-//     while (z<answerArray.length) {
-//         var answerCorrect = false
-//         if (vehicleGuess.toLowerCase === answerArray[z]){
-//             alert('yep, that is a correct answer');
-//             z = answerArray.length;
-//             answerCorrect = true;
-//             break;
-//         }
-//         else {
-//             z+=1;
-//         }
-//     }
-//     if (answerCorrect === false){
-//         alert('that answer is not correct, try again.')
-//     }
-
-// }
-
-// var answerArray = ['sedan', 'truck', 'car', 'suv', 'van'];
-// var x = 0;
-// while (x < answerArray.length) {
-//     alert(answerArray[x]);
-//     x += 1;
-// }
+alert('your total number of correct answers was ' + numberCorrect)
